750ef5d2fd078fde55df8e5aa9a19a8ad572a510
prangancode <senprangan@gmail.com>
2021-11-29 23:31:30 +0600

    first commit

---
+++ src/projectData.js
@@ -0,0 +1,28 @@
+const projects = [
+    {
+        title: 'Wheels on Wings - Niche products Web App',
+        image: './images/rn-mockup',
+        description: 'A Full stack web application where user can order bicycles online. Login system with firebase and private routes. Dashboard for users and admins where admins can do CRUD operations and users can give reviews and monitor their orders.',
+        tools: ['React', 'Firebase', 'MongoDB', 'Node', 'Express'],
+        github: 'https://github.com/prangancode/niche-bicycle-client-side-a12',
+        link: 'https://niche-product-website-6d655.web.app/',
+    },
+    {
+        title: 'TRAVELATA – Tour Agency Web App',
+        image: './images/tmdb-mockup',
+        description: 'A single page full stack traveling website. Login system with firebase and private routes.Dashboard for users and admins where admins can do CRUD operations.',
+        tools: ['React', 'Firebase', 'MongoDB', 'Node', 'Express'],
+        github: 'https://github.com/prangancode/tourism-website-client-side-a11',
+        link: 'https://tourism-website-cf4cd.web.app/',
+    },
+    {
+        title: 'EVER CARE HOSPITAL – Healthcare Website',
+        image: './images/recipeApp-mockup',
+        description: 'A modern website for hospital. Where user can see all services and find a doctor for treatment. User can sign in with email or google.',
+        tools: ['React', 'API', 'MongoDB', 'Tailwind CSS', 'Firebase'],
+        github: 'https://github.com/prangancode/healthcare-related-website-a10',
+        link: 'https://healthcare-web-98117.web.app/',
+    },
+];
+
+export default projects;
